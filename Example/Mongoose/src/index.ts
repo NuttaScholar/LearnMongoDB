727921dd3407018ebc26536cfe79784dbb231e99
NuttaScholar <nutta766@gmail.com>
2025-03-22 00:50:42 +0700

    Set Routes

--- Example/Mongoose/src/index.ts
+++ Example/Mongoose/src/index.ts
@@ -17,6 +17,7 @@ const PORT = 5000;
 /*********************************************** */
 // อนุญาตให้ React frontend สามารถส่งคำขอมาได้
 app.use(cors());
+app.use(express.json());
 
 /*********************************************** */
 // Mongoose Setup
@@ -29,14 +30,72 @@ const userSchema = new mongoose.Schema({
   name: String,
   age: Number,
 });
+const transatcionSchema = new mongoose.Schema({
+  userid: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+  title: String,
+  amount: Number,
+});
 
 // สร้าง Model
 const User = mongoose.model("user", userSchema);
+const Transatcion = mongoose.model("Transaction", transatcionSchema);
 /*********************************************** */
 // Routes Setup
 /*********************************************** */
-const newUser = new User({ name: "Alice", age: 25 });
-newUser.save().then(() => console.log("User added!"));
+app.post('/User', async (req: Request, res: Response) => {
+  try {
+    console.log(req.body);
+    const newUser = new User(req.body);
+    await newUser.save().then(() => console.log("User added!"));
+    res.send("Message received!");
+  } catch (err) {
+    res.send({ Error: err });
+  }
+})
+app.post('/Transaction', async (req: Request, res: Response) => {
+  try {
+    console.log(req.body);
+    const newTransatcion = new Transatcion(req.body);
+    await newTransatcion.save().then(() => console.log("Transatcion added!"));
+    res.send("Message received!");
+  } catch (err) {
+    res.send({ Error: err });
+  }
+})
+app.get('/User', async (req: Request, res: Response) => {
+  User.find().then((data) => {
+    res.send(data);
+  })
+})
+app.get('/Transaction', async (req: Request, res: Response) => {
+  Transatcion.find().populate("userid").limit(10).then((data) => {
+    res.send(data);
+  })
+})
+app.delete('/User', async (req: Request, res: Response) => {
+  User.deleteOne({ _id: req.body._id }).then((result) => {
+    console.log(`count: ${result.deletedCount}, acknowledged: ${result.acknowledged}`)
+    res.send(result);
+  })
+})
+app.delete('/Transaction', async (req: Request, res: Response) => {
+  console.log(`id: ${req.query._id}`)
+  Transatcion.deleteOne({ _id: req.query._id }).then((result) => {
+    console.log(`count: ${result.deletedCount}, acknowledged: ${result.acknowledged}`)
+    res.send(result);
+  })
+})
+app.put('/User', async (req: Request, res: Response) => {
+  User.updateOne({ _id: req.query._id }, req.body).then((result) => {
+    res.send(result);
+  })
+})
+app.put('/Transaction', async (req: Request, res: Response) => {
+  Transatcion.updateOne({ _id: req.query._id }, req.body).then((result) => {
+    res.send(result);
+  })
+})
+
 /*********************************************** */
 // Start Server
 /*********************************************** */
